2d954232adc30689d4ff3e9a771ee3a6c95d2fde
felipemagalhaes <felipemlopes0009@gmail.com>
2024-10-07 22:13:47 -0400

    Testing arrays, for loops, float32 and float64 and conditions

--- main.go
+++ main.go
@@ -3,5 +3,36 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("what's up")
+	myArrays()
+}
+
+func x() {
+	var v int = 20
+	var w int = 30
+
+	if v < w {
+		v = v - 10
+	} else {
+		v = v + 10
+	}
+
+	var result int = v + w
+
+	fmt.Println("RESULT: ", result)
+}
+
+func y() {
+	var z float32 = 23.3
+
+	for x := z; x >= 0.28; x = x - 1.0 {
+		fmt.Println(x)
+	}
+}
+
+func myArrays() {
+	var one [5]int // Create an array with 5 values of type integers
+
+	one[4] = 49
+
+	fmt.Println(one)
 }
