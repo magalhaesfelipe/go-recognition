76c09a149795bf5c7e9f19fded4d592b6c078c57
lopesmfelipe <felipemlopes0009@gmail.com>
2024-10-13 22:06:35 -0400

    Third function returning two different values of two different types

---
+++ course/main.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+// available anywhere inside the 'main' package/file
+var omega float64 = .9 // package level variable
+
+func main() {
+
+	var name string
+	name = "Felipe"
+	fmt.Println("The world is yours", name)
+
+	var firstNumber float64 = 30
+	firstNumber = firstNumber + omega
+	fmt.Println(firstNumber)
+
+	z := saySomething("Part 1, ")
+	fmt.Println("z variable value: ", z)
+	
+	str, num := third()
+	
+	fmt.Println(str, num)
+}
+
+func saySomething(phrase1 string) float64 {
+	
+	var phrase2 string = "Part 2"
+	var result string = phrase1 + phrase2
+	fmt.Println(result)
+	
+	return omega
+}
+
+
+func third() (string, int) { // the function returns two values, of different types
+	return "string and ", 1
+}
