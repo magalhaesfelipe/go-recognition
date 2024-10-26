cdd3a7315fb453312d85ec751607afd6a9ed64b5
magalhaesfelipe <felipemlopes0009@gmail.com>
2024-10-26 05:29:02 -0400

    Updating json

--- section2/reading-json/main.go
+++ section2/reading-json/main.go
@@ -1,5 +1,29 @@
 package make
 
+type Manga struct {
+	Name string `json:"name"`
+	Author string `json:"author"`
+	Chapters int `json:"chapters"`
+}
+
 func main() {
+
+	// JSON = Javascript Object Notation
+	mangaDataJson := `
+[
+	{
+		"name": "Ajin",
+		"author": "Unknow",
+		"chapters": 83
+	},
+	{
+		"name": "One Punch Man",
+		"author": "One, Yusuke Murata",
+		"chapters": 200
+	}	
+]`
+
+	var x 
+
 
 }
